3b4784855704fbdab27e33c693e924a7f4d9bee1
Vandeilson <vandeilsons@hotmail.com>
2026-09-23 17:36:02 -0300

    Teste da branch teste

--- server.ts
+++ server.ts
@@ -310,7 +310,7 @@ async function runAutomatedEmails(type: 'weekly' | 'daily') {
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = 5000;
 
   app.use(express.json());
 
